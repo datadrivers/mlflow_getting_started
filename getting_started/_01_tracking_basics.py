6e37f8febb5b5c42f70ab02903c3f9f78acd4920
Carsten <carsten.frommhold@datadrivers.de>
2021-07-23 12:29:46 +0200

    after workshop

--- getting_started/_01_tracking_basics.py
+++ getting_started/_01_tracking_basics.py
@@ -9,14 +9,14 @@ if __name__ == "__main__":
     mlflow.set_tracking_uri(mlflow_server_uri)
 
     # Set Experiment
-    mlflow.set_experiment("My Basic Experiment1")
+    mlflow.set_experiment("My Basic Experiment")
 
     with mlflow.start_run(run_name="My first run"):
 
-        mlflow.log_param("p1", 0.5)
-        mlflow.log_metric("m1", 0.89)
+        mlflow.log_param("my_parameter", 0.5)
+        mlflow.log_metric("my_metric", 0.89)
 
         with open("sample_artifact.txt", 'w') as f:
-            f.write("Some artifact content.")
+            f.write("Some artifact content for demo purposes.")
         mlflow.log_artifact("sample_artifact.txt", artifact_path="prefix_in_folder")
 
